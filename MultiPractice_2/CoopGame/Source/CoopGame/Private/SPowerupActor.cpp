bfe0b3a2532e60f2b951f303befdd3da249627fc
광현 최 <helirang34@gmail.com>
2021-08-16 14:57:39 +0900

    ItemUpdate_SpeedUp_End

--- MultiPractice_2/CoopGame/Source/CoopGame/Private/SPowerupActor.cpp
+++ MultiPractice_2/CoopGame/Source/CoopGame/Private/SPowerupActor.cpp
@@ -34,9 +34,13 @@ void ASPowerupActor::OnTickPowerup()
 
 void ASPowerupActor::ActivatePowerup()
 {
+	OnActivated();
+	//활성화 호출
+
 	if (PowerupInterval > 0.0f)
 	{
-		GetWorldTimerManager().SetTimer(TimerHandle_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval, true, 0.0f);
+		GetWorldTimerManager().SetTimer(TimerHandle_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval, true);
+		// 틱 제거, 즉시 발생 but 첫번째 인터페이스가 통과되야 발생 시작
 	}
 	{
 		OnTickPowerup();
